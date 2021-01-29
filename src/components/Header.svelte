c73c9ca505a2b2aa0c89cc73068baf3533eae1a9
Øystein Hodne <oystein.hodne@gmail.com>
2021-01-29 13:53:12 +0100

    Close menu on navigate mobile

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,6 +5,11 @@
   function logout() {
     auth.logout();
   }
+
+  function closeMenu(){
+    window.$('.navbar-collapse').collapse('hide');
+  }
+  
 </script>
 
 <style>
@@ -20,40 +25,38 @@
         <img src="img/logo.jpg" width="30" height="30" alt="SBB-logo" />
         Notearkiv - SBB
       </a>
-
       
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbar"
-          aria-controls="navbar"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon" />
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbar"
+        aria-controls="navbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#/" on:click={closeMenu}>Hjem</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#/archive" on:click={closeMenu}>Arkivliste</a>
+          </li>
+          {#if $isAdmin}
+            <li class="nav-item">
+              <a class="nav-link" href="#/projects" on:click={closeMenu}>Prosjekter</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#/users" on:click={closeMenu}>Brukere</a>
+            </li>
+          {/if}
+        </ul>
+        <button class="btn btn-secondary navbar-right my-2 my-lg-0" on:click={logout}>
+          Logg ut
         </button>
-        <div class="collapse navbar-collapse" id="navbar">
-
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="#/">Hjem</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#/archive">Arkivliste</a>
-            </li>
-            {#if $isAdmin}
-              <li class="nav-item">
-                <a class="nav-link" href="#/projects">Prosjekter</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#/users">Brukere</a>
-              </li>
-            {/if}
-          </ul>
-          <button class="btn btn-secondary navbar-right my-2 my-lg-0" on:click={logout}>
-            Logg ut
-          </button>
-        </div>
+      </div>
     </div>
   </nav>
 </header>
